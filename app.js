ba552fc8cc915ba1b4ef0448df932d07a3802e74
ahmedmagdy491 <ahmedahmedmagdy491@gmail.com>
2021-09-25 03:50:54 +0200

    setup server side

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
+require('dotenv/config');
 // … Configure Express, and register necessary route handlers
-const port = 4000;
+const port = process.env.PORT;
 srv = app.listen(port, () => console.log('server rinning', port));
 app.use(
 	'/peerjs',
